166ac06081aab5af7dd4e834dd393b1ccf869d91
Akshay Kumar <147413225+akshaykr3011@users.noreply.github.com>
2023-12-23 21:00:11 +0530

    Update NotePage.js

--- mynotes/src/pages/NotePage.js
+++ mynotes/src/pages/NotePage.js
@@ -67,7 +67,7 @@ const NotePage = () => {
         {id !== 'new' ? (
           <button onClick={deleteNote}>Delete</button>
         ):(
-          <button onClick={handleSubmit}>Save Note</button>
+          <button onClick={handleSubmit}>Save</button>
         )}
       </div>
       <div className="note-body">
